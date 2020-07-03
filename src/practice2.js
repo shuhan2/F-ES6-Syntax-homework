e472dab3e3bbf97402c47b693ae1d26126b7dfaf
qianwu2 <qianwu2@ap.xom.com>
2020-07-03 10:15:08 +0800

    implementation practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,18 @@
-const inject  = [];
-export { inject };
+const inject = function (items, sections) {
+  let array = [];
+  let originalIndex = 0;
+  for (let index = 0; index < sections.length; index++) {
+    let value = sections[index].content;
+    let sectionIndexValue = sections[index].index;
+    for (let i = originalIndex; i < sectionIndexValue + index; i++) {
+      let temp = originalIndex;
+      array[originalIndex++] = items[temp - index];
+    }
+    array[originalIndex++] = value;
+  }
+  for (let lastIndex = originalIndex; lastIndex < sections.length + items.length; lastIndex++) {
+    array[lastIndex] = items[lastIndex - sections.length];
+  }
+  return array;
+};
+export {inject};
